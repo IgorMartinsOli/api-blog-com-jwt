a4c5bc6ecfe3016d00fe5690cf3891c563edbd5c
Igor_ <igrmartoli@gmail.com>
2022-09-09 10:18:21 -0300

    finish login token

---
+++ src/usuarios/estrategias-autenticacao.js
@@ -0,0 +1,45 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const Usuario = require('./usuarios-modelo');
+const {InvalidArgumentError} = require('../erros');
+const bcrypt = require('bcrypt');
+const BearerStrategy = require('passport-http-bearer').Strategy;
+
+function verificaUsuario(usuario) {
+    if(!usuario){
+        throw new InvalidArgumentError('Não existe usuario com esse e-mail');
+    }
+}
+
+async function verificaSenha(senha, senhaHash) {
+    const senhaValida = await bcrypt.compare(senha, senhaHash);
+    if (!senhaValida) {
+        throw new InvalidArgumentError('E-mail ou senha inválidos!');
+    }
+}
+
+passport.use(
+    new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'senha',
+        session: false
+    },
+    async (email, senha, done) => {
+        try {
+            const usuario = await Usuario.buscaPorEmail(email);
+            console.log(usuario)
+            verificaUsuario(usuario);
+            await verificaSenha(senha, usuario.senhaHash);
+            done(null, usuario);
+        } catch (erro) {
+            done(erro);
+        }
+    }
+    )
+);
+
+passport.use(
+    new BearerStrategy(
+        
+    )
+)
